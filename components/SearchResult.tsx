5cfb9a82d9d1a327c0ecd2cd6f7919a476b628e3
Jacob Blanchard <jacobablanchard@gmail.com>
2023-02-17 13:54:05 -0600

    More changes

--- components/SearchResult.tsx
+++ components/SearchResult.tsx
@@ -1,40 +1,23 @@
-import ImageNotSupportedOutlinedIcon from '@mui/icons-material/ImageNotSupportedOutlined'
-import { Box, Stack, Typography } from '@mui/material'
-import { height } from '@mui/system'
-import Image from 'next/image'
-import * as React from 'react'
-import { useContext } from 'react'
+import ImageNotSupportedOutlinedIcon from '@mui/icons-material/ImageNotSupportedOutlined';
+import { Box, Stack, useTheme } from '@mui/material';
+import Image from 'next/image';
+import * as React from 'react';
+import { useContext } from 'react';
 
-import { ImageConfigContext } from '../hooks/useImageConfig'
-import { TVResultType } from '../server/routers/theMovieDBRouter'
+import useImageBlur from '../hooks/useImageBlur';
+import { ImageConfigContext } from '../hooks/useImageConfig';
+import { TVResultType } from '../server/routers/theMovieDBRouter';
 
 export interface ISearchResultProps {
-    result: TVResultType
-    onClick: (SelectedShow: TVResultType) => void
+    result: TVResultType;
+    onClick: (SelectedShow: TVResultType) => void;
 }
 
 export function SearchResult(props: ISearchResultProps) {
-    const imageConfig = useContext(ImageConfigContext)
-    const imageWidth = imageConfig?.images.poster_sizes[0] ?? ''
-
-    const shimmer = (w: number, h: number) => `
-<svg width="${w}" height="${h}" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-  <defs>
-    <linearGradient id="g">
-      <stop stop-color="#333" offset="20%" />
-      <stop stop-color="#222" offset="50%" />
-      <stop stop-color="#333" offset="70%" />
-    </linearGradient>
-  </defs>
-  <rect width="${w}" height="${h}" fill="#333" />
-  <rect id="r" width="${w}" height="${h}" fill="url(#g)" />
-  <animate xlink:href="#r" attributeName="x" from="-${w}" to="${w}" dur="1s" repeatCount="indefinite"  />
-</svg>`
-
-    const toBase64 = (str: string) =>
-        typeof window === 'undefined'
-            ? Buffer.from(str).toString('base64')
-            : window.btoa(str)
+    const imageConfig = useContext(ImageConfigContext);
+    const imageWidth = imageConfig?.images.poster_sizes[0] ?? '';
+    const imageBlurURL = useImageBlur(parseInt(imageWidth?.slice(1)), 125);
+    const theme = useTheme();
 
     return (
         <Stack
@@ -42,6 +25,11 @@ export function SearchResult(props: ISearchResultProps) {
             alignItems={'center'}
             spacing={1}
             onClick={() => props.onClick(props.result)}
+            sx={{
+                '&:hover': {
+                    outline: `1px solid ${theme.palette.warning.dark}`,
+                },
+            }}
         >
             <Box>
                 {props.result.poster_path ? (
@@ -51,9 +39,7 @@ export function SearchResult(props: ISearchResultProps) {
                         width={parseInt(imageWidth?.slice(1))}
                         height={125}
                         placeholder="blur"
-                        blurDataURL={`data:image/svg+xml;base64,${toBase64(
-                            shimmer(parseInt(imageWidth?.slice(1)), 125)
-                        )}`}
+                        blurDataURL={imageBlurURL}
                     />
                 ) : (
                     <ImageNotSupportedOutlinedIcon
@@ -66,5 +52,5 @@ export function SearchResult(props: ISearchResultProps) {
             </Box>
             <div>{props.result.name} </div>
         </Stack>
-    )
+    );
 }
--- components/SelectedShow.tsx
+++ components/SelectedShow.tsx
@@ -1,12 +1,36 @@
-import { Typography } from '@mui/material'
-import * as React from 'react'
+import { Stack, Typography } from '@mui/material';
+import Image from 'next/image';
+import * as React from 'react';
+import { useContext } from 'react';
 
-import { TVResultType } from '../server/routers/theMovieDBRouter'
+import useImageBlur from '../hooks/useImageBlur';
+import { ImageConfigContext } from '../hooks/useImageConfig';
+import { TVResultType } from '../server/routers/theMovieDBRouter';
 
 export interface ISelectedShowProps {
-    show: TVResultType | null
+    show: TVResultType | null;
 }
 
 export function SelectedShow(props: ISelectedShowProps) {
-    return <Typography>{props.show?.name}</Typography>
+    const imageConfig = useContext(ImageConfigContext);
+    const imageWidth = imageConfig?.images.backdrop_sizes[2] ?? '';
+    const imageBlurURL = useImageBlur(parseInt(imageWidth?.slice(1)), 250);
+
+    return (
+        props.show && (
+            <Stack position={'relative'} padding={1}>
+                <Typography>{props.show?.name}</Typography>
+                <Image
+                    src={`${imageConfig?.images.secure_base_url}${imageWidth}${props.show?.backdrop_path}`}
+                    alt={`${props.show.name} backdrop`}
+                    fill
+                    placeholder="blur"
+                    blurDataURL={imageBlurURL}
+                    style={{
+                        objectFit: 'scale-down',
+                    }}
+                />
+            </Stack>
+        )
+    );
 }
